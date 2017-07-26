e7ecfefef0a6b6ad06cf33279ba3b1be2498987a
MichaeAllen1966 <michael.allen1966@gmail.com>
2017-07-26 13:48:28 +0100

    Version 0.1

--- README.md
+++ README.md
@@ -1,4 +1,80 @@
 # hospital_travel_and_admissions
+================================
 
-General code to calculate travel time/distances from  LSOA centroids to acute hospitals, with admission numbers per hospital
+General code to calculate travel time/distances from LSOA centroids to acute hospitals, with
+admission numbers per hospital.
 
+
+Data
+====
+In the data subfolder are four files:
+
+1) admissions_by LSOA. This is the yearly admissions to any hospital by LSOA. If this table is
+incomplete then the code will fill in missing values with zeros. Order of this table is not
+important; the code will sort the order.
+
+2) hospitals. This table contains all acute hospitals. The order of the hospitals must not be
+altered and no hospitals can be added or removed. The forth column of the table allows hospitals
+to be selected as being able to offer the service under investigation. This column should have a
+value of 1 (hospital is open for service) or 0 (hospital is closed). The code truncates the hospital
+list just to those hospitals with a 1.
+
+3) travel_matrix_miles. DO NOT CHANGE THIS FILE. This contains estimated road travel distances (from
+Maptitude 2016 with MP-MileCharter add-in) from all LSOAs to all acute hospitals.
+
+4) travel_matrix_minutes. DO NOT CHANGE THIS FILE. This contains estimated road travel times (from
+Maptitude 2016 with MP-MileCharter add-in) from all LSOAs to all acute hospitals.
+
+
+Running the code
+================
+The output folder for results needs to be set. This is set in the first line of the main() function,
+which is placed first in the list of functions (all other functions are ordered alphabetically)
+
+By default the travel matrix loaded is travel times, not distances. This may be changed in the
+load_data function.
+
+
+Output files
+============
+The following output files are generated:
+
+1) binned_admissions_by_distance.csv - this bins admissions by 15 minute or 15 mile bins by default.
+The bin value is set in the first line of the summarise_data_by_hospital function. This counts
+admissions by time/distance bin. This is not broken down by hospital attended.
+
+2) binned_admissions_by_hospital_and_distance.csv - as (1) but broken dowm by hospital attended.
+
+3) global_results.csv - total admissions and overall average travel time/distance
+
+4) results_by_LSOA.csv - admissions, closest hospital and travel time or distance by LSOA.
+
+5) summary_by_hospital.csv - admissions to each hospital with average travel time for people
+travelling to that hospital.
+
+
+Licence
+=======
+
+This code is provided under a permissive Open Source Licence:
+
+Copyright 2017 Michael J Allen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
--- run_analysis.py
+++ run_analysis.py
@@ -3,53 +3,81 @@ Analysis of patient travel distances and hospital admissions
 
 Copyright 2017 Michael J Allen
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of 
-this software and associated documentation files (the "Software"), to deal in 
-the Software without restriction, including without limitation the rights to 
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
-of the Software, and to permit persons to whom the Software is furnished to do 
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
 so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-
 """
 
-import ipdb # Used during dev to trace errors: ipdb.set_trace()
-ipdb.set_trace()
+#import ipdb # Used during dev to trace errors: ipdb.set_trace()
+#ipdb.set_trace()
 
 
 # Load libraries
+print('Loading libraries')
 import numpy as np
 import pandas as pd
 import os, sys
+#import ipdb; ipdb.set_trace()
+
+def main():
+    """Main code. Note: All data is stored in Data class. Functions access data in Data class
+    directly"""
+    # ***************** SET OUTPUT LOCATION BELOW ******************
+    Data.OUTPUT_LOCATION = 'output/test'
+    # Main code
+    create_output_folder(Data.OUTPUT_LOCATION)
+    load_data()
+    align_admissions_and_travel_matrix_data()
+    check_hospital_list()
+    truncate_data_to_available_hospitals()
+    identify_closest_hospital()
+    combine_admissions_with_closest_hospital()
+    summarise_data_by_hospital()
+    save_results()
+    print('end')
 
 class Data:
     """Data class stores all global data.
-    All data is stored in the parent class. No other individual instances of the class exist"""
-    OUTPUT_LOCATION = 'output/test'
+    All data is stored in the parent class. No other individual instances of the class exist.
+	Data strored:
+		OUTPUT_LOCATION - folder location for output
+		admissions_by_lsoa - number of admission by lsoa (dorted to match travel matric)
+		hospitals - list of hospitals amrked for whether required specialty is present
+		loaded_admissions_per_LSOA - loaded admission by LSOA. Deleted after sorted and stored in
+            admissions_by_lsoa
+		travel_matrix - distance or time between all LSOAs and all hospitals
+		"""
 
 def align_admissions_and_travel_matrix_data():
-    # Ensure admissions by LSOA are in same order as travel matrix and replace any missing values with 0
+    """Ensure admissions by LSOA are in same order as travel matrix and replace any missing values
+    with 0"""
+    print('Aligning admissions and travel mattrix LSOAs')
     travel_matrix_patient_lsoa = pd.DataFrame(Data.travel_matrix.index)
     travel_matrix_patient_lsoa = travel_matrix_patient_lsoa.set_index('LSOA')
-    admissions_by_lsoa = pd.concat([travel_matrix_patient_lsoa, loaded_admissions_per_LSOA], axis=1,
-                                   join_axes=[travel_matrix_patient_lsoa.index])
+    Data.admissions_by_lsoa = pd.concat([travel_matrix_patient_lsoa,
+        Data.loaded_admissions_per_LSOA], axis=1,join_axes=[travel_matrix_patient_lsoa.index])
     Data.admissions_by_lsoa.fillna(0,inplace=True)
     # Remove loaded admission data now data has been aligned
     del Data.loaded_admissions_per_LSOA
     return()
 
 def check_hospital_list():
-    # Check hospital list is same as in travel matrix. Exit if not.
+    """Check hospital list is same as in travel matrix. Exit if not."""
+    print('Checking hospital list matches travel matrix hospitals')
     check_1 = list(Data.travel_matrix) # postcodes of hospitals in travel matrix
     check_2 = Data.hospitals.index.tolist() # postcodes of hospitals in hospital list
     if check_1 != check_2:
@@ -57,34 +85,100 @@ def check_hospital_list():
         sys.exit()
     return()
 
+def combine_admissions_with_closest_hospital():
+    """Create single table of LSOAs with admissions, closesy hopsital postocde and closest hospital
+    distance"""
+    print('Collating data')
+    Data.results_by_LSOA = pd.concat([Data.admissions_by_lsoa,Data.closest_hospital_postcode,
+                                      Data.closest_hospital_distance],axis=1)
+    return()
+
 def create_output_folder(output_folder):
     """If output path and folder do not exist, create them"""
+    print('Creating output folder')
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
     return()
 
+def identify_closest_hospital():
+    """Identifiesd the closest hospital and distance cto closest hospital for each LSOA"""
+    print('Identifying the closest available hospital for each LSOA')
+    Data.closest_hospital_postcode=pd.DataFrame()
+    Data.closest_hospital_postcode['closest_hospital_postcode'] = Data.travel_matrix.idxmin(axis=1)
+    Data.closest_hospital_distance=pd.DataFrame()
+    Data.closest_hospital_distance['closest_hospital_distance'] = Data.travel_matrix.min(axis=1)
+    return()
+
 def load_data():
+    """Load data"""
+    print('Loading data')
     Data.loaded_admissions_per_LSOA = pd.read_csv('data/admissions_by_lsoa.csv',index_col=0)
-    Data.travel_matrix = pd.read_csv('data/travel_matrix.csv',index_col=0)
+    Data.travel_matrix = pd.read_csv('data/travel_matrix_minutes.csv',index_col=0)
     Data.hospitals=pd.read_csv('data/hospitals.csv',index_col=0)
+    return()
 
-def main():
-    create_output_folder(Data.OUTPUT_LOCATION)
-    load_data()
-    align_admissions_and_travel_matrix_data()
+def save_results():
+    """Save results to previously defined output location"""
+    print('Saving results')
+    # Round results prior to Saving
+    Data.summary_by_hospital = Data.summary_by_hospital.round(decimals=2)
+    Data.summary_by_hospital.to_csv(Data.OUTPUT_LOCATION+'/summary_by_hospital.csv')
+    Data.binned_admissions_by_hospital_and_distance = (
+        Data.binned_admissions_by_hospital_and_distance.round(decimals=2))
+    (Data.binned_admissions_by_hospital_and_distance.to_csv(Data.OUTPUT_LOCATION+
+            '/binned_admissions_by_hospital_and_distance.csv'))
+    Data.binned_admissions_by_distance = Data.binned_admissions_by_distance.round(decimals=2)
+    (Data.binned_admissions_by_distance.to_csv(Data.OUTPUT_LOCATION+
+            '/binned_admissions_by_distance.csv'))
+    Data.global_results = Data.global_results.round(decimals = 2)
+    Data.global_results.to_csv(Data.OUTPUT_LOCATION+'/global_results.csv')
+    Data.results_by_LSOA = Data.results_by_LSOA.round(decimals=2)
+    Data.results_by_LSOA.to_csv(Data.OUTPUT_LOCATION+'/results_by_LSOA.csv')
+    return()
 
+
+def summarise_data_by_hospital():
+    """Summarise admission numbers and distances by hospital. Weighted distances are used for
+    averaging travel distances - for each LSOA the average distance is multiplied by the number of
+    admissions. The average disatnce for each hospital is then the sum of weighted distances divided
+    by the number fo admssions"""
+    print('Summarising data by hospital')
+    _data = Data.results_by_LSOA
+    _data['weighted_distance'] = _data['admissions']*_data['closest_hospital_distance']
+    _data['binned_distance'] = _data['closest_hospital_distance']/15
+    _data['binned_distance'] = (_data['binned_distance'].astype(int) * 15) + 15
+    grouped = _data.groupby(['closest_hospital_postcode'])
+    Data.summary_by_hospital = pd.DataFrame()
+    Data.summary_by_hospital['admissions'] = grouped['admissions'].sum()
+    Data.summary_by_hospital['average_distance'] = (grouped['weighted_distance'].sum() /
+                                                    grouped['admissions'].sum())
+    Data.binned_admissions_by_hospital_and_distance = (pd.pivot_table(_data,
+                                         values='admissions',
+                                         index=['closest_hospital_postcode'],
+                                         columns=['binned_distance'],
+                                         aggfunc=np.sum))
+    # Create global summary (not grouped by hospital attended)
+    Data.binned_admissions_by_distance = (pd.pivot_table(_data,
+                                             values='admissions',
+                                             columns=['binned_distance'],
+                                             aggfunc=np.sum))
+    Data.global_results=pd.Series()
+    Data.global_results['total_admissions'] = _data['admissions'].sum()
+    Data.global_results['average_distance'] = (_data['weighted_distance'].sum()/
+                                               _data['admissions'].sum())
+
+    return()
+
+def truncate_data_to_available_hospitals():
+    """Truncates hospital list and travel matrix to only those hospitals marked as available in the
+    hospital list"""
+    print('Truncating data to available hospitals')
+    # Truncate hopsital table
+    Data.hospitals = Data.hospitals.loc[Data.hospitals['provider']==1]
+    # Get available hospital postcodes and truncate travel matrix
+    hospital_list = Data.hospitals.index
+    Data.travel_matrix = Data.travel_matrix[hospital_list]
+    return()
 
 if __name__ == "__main__":
     main()
-
-
-
-
-# Check hospital list is same as in travel matrix. Exit if not.
-check_1 = list(travel_matrix) # postcodes of hospitals in travel matrix
-check_2 = hospitals.index.tolist() # postcodes of hospitals in hospital list
-if check_1 != check_2:
-    print('Hospital list does not match travel matrix. Quitting')
-    sys.exit()
-del check_1,check_2
-
